bcac786161a909b35868d2e659e3d7ea99751b45
Romain <93086620+Tadeusz-RZK@users.noreply.github.com>
2025-03-03 07:33:11 +0100

    Removing PyCharm default scripts comments

--- script.py
+++ script.py
@@ -1,7 +1,3 @@
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
